d5a17c86b7a415d1b837c18027abc1a8bba75ff1
SBAhsan <ahsangustavo90@gmail.com>
2026-06-13 12:53:39 +0600

    created middleware authorization token

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,13 +1,19 @@
-import type { NextFunction, Request, Response } from "express"
+import type { NextFunction, Request, Response } from "express";
 
 const authMiddleware = () => {
-    return async (req: Request, res: Response, next: NextFunction) => {
-        console.log(req.headers.authorization);
+  return async (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.headers.authorization);
 
-        
-        next();
+    const token = req.headers.authorization;
+
+    if (!token) {
+      res.status(401).json({
+        success: false,
+        message: "Unauthorized access!",
+      });
     }
+    next();
+  };
 };
-
 
 export default authMiddleware;
