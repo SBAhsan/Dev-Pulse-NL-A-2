4d454ba4c74d2bc0e1bc10b838981cfc826113db
SBAhsan <ahsangustavo90@gmail.com>
2026-06-15 00:24:31 +0600

    generated access token with refresh token

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,24 +1,24 @@
-import type { Request, Response } from "express"
-import { authService } from "./auth.service"
+import type { Request, Response } from "express";
+import { authService } from "./auth.service";
 
 const loginUser = async (req: Request, res: Response) => {
-    try {
-        const result = await authService.loginUserInDB(req.body);
+  try {
+    const result = await authService.loginUserInDB(req.body);
 
-        const {refreshToken} = result;
+    const { refreshToken } = result;
 
-        res.cookie("refreshToken", refreshToken, {
-            secure: false,
-            httpOnly: true,
-            sameSite: 'lax'
-        })
+    res.cookie("refreshToken", refreshToken, {
+      secure: false,
+      httpOnly: true,
+      sameSite: "lax",
+    });
 
-        res.status(200).json({
-            success: true,
-            message: "User logged in successfully",
-            data: result
-        })
-    } catch (error: any) {
+    res.status(200).json({
+      success: true,
+      message: "User logged in successfully",
+      data: result,
+    });
+  } catch (error: any) {
     res.status(500).json({
       success: false,
       message: error.message,
@@ -27,11 +27,27 @@ const loginUser = async (req: Request, res: Response) => {
   }
 };
 
-
 const refreshToken = async (req: Request, res: Response) => {
-    console.log(req.cookies);
-}
+  try {
+    const result = await authService.generateRefreshToken(
+      req.cookies.refreshToken,
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Access token generated",
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      error: error.detail,
+    });
+  }
+};
 
 export const authController = {
-    loginUser, refreshToken
-}
+  loginUser,
+  refreshToken,
+};
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -2,7 +2,7 @@ import config from "../../config";
 import { pool } from "../../db";
 import type { IUserLogin } from "./auth.interface";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+import jwt, { type JwtPayload } from "jsonwebtoken";
 
 const loginUserInDB = async (payload: IUserLogin) => {
   const { email, password } = payload;
@@ -44,6 +44,45 @@ const loginUserInDB = async (payload: IUserLogin) => {
   return { accessToken, refreshToken };
 };
 
+const generateRefreshToken = async (token: string) => {
+  if (!token) {
+    throw new Error("Unauthorized access!");
+  }
+
+  const decodeToken = jwt.verify(
+    token as string,
+    config.refresh_key,
+  ) as JwtPayload;
+
+  const userData = await pool.query(
+    `
+        SELECT * FROM users
+        WHERE email=$1
+        `,
+    [decodeToken.email],
+  );
+
+  if (userData.rows.length === 0) {
+    throw new Error("User not found.");
+  }
+
+  const user = userData.rows[0];
+
+  const jwtPayload = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+  };
+
+  const accessToken = jwt.sign(jwtPayload, config.access_key, {
+    expiresIn: config.access_token_expires_in as any,
+  });
+
+  return { accessToken };
+};
+
 export const authService = {
   loginUserInDB,
+  generateRefreshToken,
 };
